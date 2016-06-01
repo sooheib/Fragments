0d3c770fa4b94f8a27e5b840744f33c652a6bcb2
sooheib <soosel93@gmail.com>
2016-06-01 21:45:51 +0100

    update fragments

--- app/src/main/java/com/example/sooheib/fragments/FragmentA.java
+++ app/src/main/java/com/example/sooheib/fragments/FragmentA.java
@@ -1,9 +1,12 @@
 package com.example.sooheib.fragments;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,95 +21,72 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class FragmentA extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public FragmentA() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentA.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentA newInstance(String param1, String param2) {
-        FragmentA fragment = new FragmentA();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public void onAttach(Activity activity){
+        super.onAttach(activity);
+        Log.d("Fragment_App_Fragment","Fragment onAttach Called");
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        Log.d("Fragment_App_Fragment", "Fragment onCreate Called");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d("Fragment_App_Fragment", "Fragment onCreateView Called");
+        View v = inflater.inflate(R.layout.fragment_a, container, false);
+        return v;
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
-        View v=inflater.inflate(R.layout.fragment_a,container,false);
-
-return v;
-
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d("Fragment_App_Fragment", "Fragment onActivityCreated Called");
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void onStart() {
+        super.onStart();
+        Log.d("Fragment_App_Fragment", "Fragment onStart Called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("Fragment_App_Fragment", "Fragment onResume Called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d("Fragment_App_Fragment", "Fragment onPause Called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d("Fragment_App_Fragment", "Fragment onStop Called");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d("Fragment_App_Fragment", "Fragment onDestroyView Called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("Fragment_App_Fragment", "Fragment onDestroy Called");
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        Log.d("Fragment_App_Fragment", "Fragment onDetach Called");
     }
 }
--- app/src/main/java/com/example/sooheib/fragments/MainActivity.java
+++ app/src/main/java/com/example/sooheib/fragments/MainActivity.java
@@ -1,22 +1,55 @@
 package com.example.sooheib.fragments;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       FragmentA fragmentA=new FragmentA();
-      /*  FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.layoutMain,fragmentA,"MyFragment");
-        fragmentTransaction.commit();*/
-        //getFragmentManager().beginTransaction().add(R.id.layoutMain, fragmentA, "MyFragment").commit();
+        Log.e("Fragment_App_Activity", "Activity onCreate Called");
+
+
+        FragmentA fragmentA = new FragmentA();
+//        FragmentManager fragmentManager = getFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.add(R.id.layoutMain, fragmentA, "MyFragment");
+//        fragmentTransaction.commit();
+
+//        getFragmentManager().beginTransaction().add(R.id.layoutMain, fragmentA, "MyFragment").commit();
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("Fragment_App_Activity", "Activity onStart Called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("Fragment_App_Activity", "Activity onResume Called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("Fragment_App_Activity", "Activity onPause Called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("Fragment_App_Activity", "Activity onStop Called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("Fragment_App_Activity", "Activity onDestroy Called");
+    }
+
 }
